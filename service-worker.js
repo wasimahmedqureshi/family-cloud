18c9b0ada7535a21b73a15dce1734c59e5a9a5db
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-02-28 14:33:54 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'family-cloud-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/login.html',
+    '/dashboard.html',
+    '/album.html',
+    '/css/style.css',
+    '/js/auth.js',
+    '/js/album.js',
+    '/js/offline.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(() => {
+                    // Return offline page if fetch fails
+                    if (event.request.mode === 'navigate') {
+                        return caches.match('/index.html');
+                    }
+                });
+            })
+    );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
