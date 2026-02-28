f06ae9f96875f562727aea7c7b35e12ef0be1f68
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-02-28 14:49:06 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// js/config.js
+const DRIME_CONFIG = {
+    ACCESS_TOKEN: 'YOUR_API_KEY_HERE', // ऊपर ली गई API key यहाँ डालें
+    API_BASE_URL: 'https://api.drime.cloud/v1', // Drime का असल API endpoint
+    WORKSPACE_ID: '', // खाली छोड़ें, auto-detect हो जाएगा
+    ROOT_FOLDER: 'FamilyCloud' // आपकी फोटो वाली फोल्डर
+};
